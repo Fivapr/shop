0212d6df23a7632165eede54de625205b191486c
Fivapr <aintensifies@gmail.com>
2019-02-18 00:36:52 +0700

    roflan commit

--- server/src/hello.go
+++ server/src/hello.go
@@ -1,43 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
+	"net/http"
 )
 
-func handleConnection(conn net.Conn) {
-	name := conn.RemoteAddr().String()
-
-	fmt.Printf("%+v connected\n", name)
-	conn.Write([]byte("Hello, " + name + "\n\r"))
-
-	defer conn.Close()
-
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		text := scanner.Text()
-		if text == "Exit" {
-			conn.Write([]byte("Bye\n\r"))
-			fmt.Println(name, "disconnected")
-			break
-		} else if text != "" {
-			fmt.Println(name, "enters", text)
-			conn.Write([]byte("You enter " + text + "\n\r"))
-		}
-	}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Привет, мир!")
+	w.Write([]byte("!!!"))
 }
 
 func main() {
-	listner, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		panic(err)
-	}
-	for {
-		conn, err := listner.Accept()
-		if err != nil {
-			panic(err)
-		}
-		go handleConnection(conn)
-	}
+	http.HandleFunc("/", handler)
+
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
 }
