c8328ca790347ebaf053bb483bbcb8133c189ee8
Fivapr <aintensifies@gmail.com>
2019-02-17 10:09:16 +0700

    random stuff

--- server/hello.go
+++ server/hello.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s",
+		e.When, e.What)
+}
+
+func run() error {
+	return &MyError{
+		time.Now(),
+		"it didn't work",
+	}
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
 }
